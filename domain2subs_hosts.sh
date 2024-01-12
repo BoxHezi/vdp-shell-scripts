39827937c3cf8dc79007d1c68b2fa8defac8b22b
boxhezi <547181293@qq.com>
2024-01-12 11:43:08 +1100

    add pipe support; add time taken

--- domain2subs_hosts.sh
+++ domain2subs_hosts.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+printTimeTaken() {
+    local tip=$1
+    local start=$2
+    local end=$3
+    local findingNum=$4
+    local taken=$((end - start))
+
+    echo "Find $findingNum $tip in $(gdate -d@$taken -u +%H:%M:%S) seconds"
+}
+
 find_subs() {
     domain=$1
     outname=$2
 
     while true; do
+        startTime=$(gdate +%s)
         echo "$domain" | subfinder -silent | tee "$outname"
         if [ -s "$outname" ]; then
+            endTime=$(gdate +%s)
+            findingNum=$(wc <"$outname" -l)
+            printTimeTaken "subdomains" "$startTime" "$endTime" "$findingNum" | tee -a "$outname"
             # return if file is not empty
             return 1
         fi
@@ -18,36 +32,60 @@ find_hosts() {
     outname=$2
 
     while true; do
+        startTime=$(gdate +%s)
         dnsx -resp -silent <"$subs" | tee "$outname"
         if [ -s "$outname" ]; then
+            endTime=$(gdate +%s)
+            findingNum=$(wc <"$outname" -l)
+            printTimeTaken "hosts" "$startTime" "$endTime" "$findingNum" | tee -a "$outname"
             # return if file is not empty
             return 1
         fi
     done
 }
 
-main() {
+fromFile() {
+    input=$1
     while IFS= read -r t; do
-        subs_outname="subs_${t}.txt"
-        hosts_outname="hosts_${t}.txt"
+        echo "Processing target: $t"
+        local subs_outname="subs_${t}.txt"
+        local hosts_outname="hosts_${t}.txt"
 
         find_subs "$t" "$subs_outname"
         find_hosts "$subs_outname" "$hosts_outname"
         echo ""
-    done < <(awk -F ' ' '{print $1}' "$targets")
+    done < <(awk -F ' ' '{print $1}' "$input")
 }
 
-# read command line argument
-targets=$1
+fromPipe() {
+    for ((i = 1; i <= $#; i++)); do
+        domain="${!i}"
+        echo "Processing target: $domain"
+        local subs_outname="subs_${domain}.txt"
+        local hosts_outname="hosts_${domain}.txt"
 
-# check if targets is provided
-if [ -z "$targets" ]; then
-    echo "Please provide a file containing target domains."
-    exit 1
-# check if file exists
-elif [ ! -f "$targets" ]; then
-    echo "File $targets does not exist."
-    exit 1
+        find_subs "$domain" "$subs_outname"
+        find_hosts "$subs_outname" "$hosts_outname"
+
+        echo ""
+    done
+}
+
+if [ ! -t 0 ]; then
+    declare -a targets
+    while IFS= read -r line; do
+        targets+=("$line")
+    done
+    fromPipe "${targets[@]}"
 else
-    main
+    targetsFile=$1
+    if [ -z "$targetsFile" ]; then
+        echo "Useage: $0 <input file>"
+        exit 1
+    elif [ ! -f "$targetsFile" ]; then
+        echo "Error: $targetsFile does not exist or is not a file."
+        exit 1
+    else
+        fromFile "$targetsFile"
+    fi
 fi
